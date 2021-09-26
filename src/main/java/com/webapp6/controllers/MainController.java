691bc6f473fba22660a10b5308393bed8417b053
kofavov <kofavov@gmail.com>
2021-09-26 23:52:57 +0300

    doIt 6 урок (8:00) Добавил использование модели в контроллер Изменил index.jsp для отображения msg (синтаксис${msg})

--- src/main/java/com/webapp6/controllers/MainController.java
+++ src/main/java/com/webapp6/controllers/MainController.java
@@ -1,12 +1,14 @@
 package com.webapp6.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
     @GetMapping("/")
-    public String view(){
+    public String view(Model model){
+        model.addAttribute("msg","Hello!");
         return "/index";
     }
 }
--- src/main/webapp/WEB-INF/view/index.jsp
+++ src/main/webapp/WEB-INF/view/index.jsp
@@ -1,5 +1,8 @@
 <html>
+<head>
+    <title>Start page</title>
+</head>
 <body>
-<h2>Hello World!</h2>
+<h2>${msg}</h2>
 </body>
 </html>
