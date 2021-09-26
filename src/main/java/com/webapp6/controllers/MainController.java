059a4f865de25b4cf74d7c504042cdf13b44f37b
kofavov <kofavov@gmail.com>
2021-09-27 01:26:13 +0300

    index.jsp не работает корректно в браузере отображается ${msg}

--- src/main/java/com/webapp6/controllers/MainController.java
+++ src/main/java/com/webapp6/controllers/MainController.java
@@ -1,14 +1,20 @@
 package com.webapp6.controllers;
 
 import org.springframework.stereotype.Controller;
+
+
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class MainController {
-    @GetMapping("/")
-    public String view(Model model){
-        model.addAttribute("msg","Hello!");
-        return "/index";
+    @GetMapping("/view")
+    public String view(@RequestParam(value = "name",
+            required = false,
+            defaultValue = "user") String name, Model model){
+        model.addAttribute("name",name);
+        return "/view";
     }
 }
---
+++ src/main/webapp/WEB-INF/index.jsp
@@ -0,0 +1,10 @@
+
+<%@page contentType="text/html; charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Start page</title>
+</head>
+<body>
+<h2>Hello ${name}</h2>
+</body>
+</html>
