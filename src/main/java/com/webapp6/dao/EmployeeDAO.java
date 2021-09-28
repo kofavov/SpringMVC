258718bb902db4c48b81107317c604a2b078ba2a
kofavov <kofavov@gmail.com>
2021-09-28 16:54:31 +0300

    в методах стал использовать jdbcTemplate старый способ закоментил если его снова использовать вернуть проброс sqlexeption

--- src/main/java/com/webapp6/dao/EmployeeDAO.java
+++ src/main/java/com/webapp6/dao/EmployeeDAO.java
@@ -1,6 +1,9 @@
 package com.webapp6.dao;
 
 import com.webapp6.models.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
@@ -12,74 +15,60 @@ import java.util.Properties;
 
 @Component
 public class EmployeeDAO {
-    private static Connection connection;
+    private final JdbcTemplate jdbcTemplate;
 
-    static {
-        String url = null;
-        String username = null;
-        String password = null;
-
-        try(InputStream inputStream = EmployeeDAO.class.getClassLoader().getResourceAsStream("persistence.properties")) {
-            Properties properties = new Properties();
-            properties.load(inputStream);
-            url = properties.getProperty("url");
-            username=properties.getProperty("username");
-            password=properties.getProperty("password");
-        }catch (IOException ioException){
-            ioException.printStackTrace();
-        }
-
-        try {
-            Class.forName("org.postgresql.Driver");
-            connection = DriverManager.getConnection(url,username,password);
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+    @Autowired
+    public EmployeeDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
     }
 
-    public List<Employee> getEmployees() throws SQLException {
-        List<Employee> employees = new ArrayList<>();
-        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM employee");
-        ResultSet resultSet = preparedStatement.executeQuery();
-        while (resultSet.next()) {
-            Employee employee = new Employee();
-            employee.setId(resultSet.getInt(1));
-            employee.setFio(resultSet.getString(2));
-            employee.setCompanyid(resultSet.getInt(3));
-            employee.setSalary(resultSet.getInt(4));
-            employees.add(employee);
-        }
-        return employees;
+    public List<Employee> getEmployees()  {
+//        List<Employee> employees = new ArrayList<>();
+//
+//        PreparedStatement preparedStatement = connection.prepareStatement();
+//        ResultSet resultSet = preparedStatement.executeQuery();
+//        while (resultSet.next()) {
+//            Employee employee = new Employee();
+//            employee.setId(resultSet.getInt(1));
+//            employee.setFio(resultSet.getString(2));
+//            employee.setCompanyid(resultSet.getInt(3));
+//            employee.setSalary(resultSet.getInt(4));
+//            employees.add(employee);
+//        }
+        return jdbcTemplate.query("SELECT * FROM employee",new BeanPropertyRowMapper<>(Employee.class));
     }
 
     public Employee getOneById(int id){
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * from employee where id = ?");
-            preparedStatement.setInt(1,id);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            if (resultSet.next()){
-                Employee employee = new Employee();
-                employee.setId(resultSet.getInt(1));
-                employee.setFio(resultSet.getString(2));
-                employee.setCompanyid(resultSet.getInt(3));
-                employee.setSalary(resultSet.getInt(4));
-                return employee;
-            }
-        } catch (SQLException e) {
-
-        }
-        return null;
+//        try {
+//            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * from employee where id = ?");
+//            preparedStatement.setInt(1,id);
+//            ResultSet resultSet = preparedStatement.executeQuery();
+//            if (resultSet.next()){
+//                Employee employee = new Employee();
+//                employee.setId(resultSet.getInt(1));
+//                employee.setFio(resultSet.getString(2));
+//                employee.setCompanyid(resultSet.getInt(3));
+//                employee.setSalary(resultSet.getInt(4));
+//                return employee;
+//            }
+//        } catch (SQLException e) {
+//
+//        }
+        return jdbcTemplate.query(
+                "SELECT * from employee where id = ?",new Object[]{id},new BeanPropertyRowMapper<>(Employee.class))
+                .stream().findAny().orElse(null);
     }
 
-    public void add(Employee employee) throws SQLException {
-        PreparedStatement preparedStatement =
-                connection.prepareStatement
-                        ("INSERT into employee values (?,?,?,?)");
-        preparedStatement.setInt(1,employee.getId());
-        preparedStatement.setString(2,employee.getFio());
-        preparedStatement.setInt(3,employee.getCompanyid());
-        preparedStatement.setInt(4,employee.getSalary());
-        preparedStatement.execute();
+    public void add(Employee employee)  {
+//        PreparedStatement preparedStatement =
+//                connection.prepareStatement
+//                        ("INSERT into employee values (?,?,?,?)");
+//        preparedStatement.setInt(1,employee.getId());
+//        preparedStatement.setString(2,employee.getFio());
+//        preparedStatement.setInt(3,employee.getCompanyid());
+//        preparedStatement.setInt(4,employee.getSalary());
+//        preparedStatement.execute();
+        jdbcTemplate.update("insert  into employee values (?,?,?,?)",employee.getId(),employee.getFio(),employee.getCompanyid(),employee.getSalary());
     }
 
 
